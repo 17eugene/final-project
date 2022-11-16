48ec70f1788d356e0f4e554af874fa01df96ff0f
17eugene <drivercoua@gmail.com>
2022-11-16 04:22:13 +0200

    update jsx

--- client/src/components/_pages/NotFoundPage/NotFoundPage.tsx
+++ client/src/components/_pages/NotFoundPage/NotFoundPage.tsx
@@ -1,22 +1,15 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import {useTranslation} from "react-i18next";
+import { useTranslation } from "react-i18next";
 
 import "../../../styles/NotFoundPage/NotFoundPage.scss";
 
 const NotFoundPage = () => {
-  const navigate = useNavigate();
-  const {t} = useTranslation();
-
-  useEffect(() => {
-    setTimeout(() => {
-      navigate("/", { replace: true });
-    }, 4443000);
-  }, [navigate]);
+  const { t } = useTranslation();
 
   return (
     <div className="not-found-page">
       <h1 className="not-found-page__title">{t("notFound")}</h1>
+      <div className="not-found-page__img"></div>
+      
     </div>
   );
 };
