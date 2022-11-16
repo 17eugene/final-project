994a7db0cded5981c909e28df0d04450c4b02a38
17eugene <drivercoua@gmail.com>
2022-11-16 15:44:44 +0200

    add loader

--- client/src/components/OrderDetails/OrderDetails.tsx
+++ client/src/components/OrderDetails/OrderDetails.tsx
@@ -3,6 +3,7 @@ import { useAppSelector } from "../../redux/hooks/hooks";
 import { useTranslation } from "react-i18next";
 import CarsElement from "../CarsElement/CarsElement";
 import OrderDetailsItem from "../OrderDetailsItem/OrderDetailsItem";
+import Loader from "../Loader/Loader";
 
 import ThemeContext from "../../context/context";
 
@@ -13,23 +14,30 @@ const OrderDetails = () => {
   const ordersCollection = useAppSelector(
     (state) => state.orders.ordersCollection
   );
+  const ordersLoading = useAppSelector((state) => state.orders.loading);
   const { t } = useTranslation();
 
   return (
     <div className={theme === "light" ? "order-details" : "order-details dark"}>
-      {ordersCollection.length > 0 ? (
-        ordersCollection.map((order) => (
-          <OrderDetailsItem key={order._id}>
-            <CarsElement
-              carId={order.orderedCar.car}
-              dateStart={order.dateStart}
-              dateEnd={order.dateEnd}
-              totalPrice={order.orderedCar.totalPrice}
-            />
-          </OrderDetailsItem>
-        ))
+      {ordersLoading ? (
+        <Loader />
       ) : (
-        <p className="order-details__empty">{t("order.empty")}</p>
+        <>
+          {ordersCollection.length > 0 ? (
+            ordersCollection.map((order) => (
+              <OrderDetailsItem key={order._id}>
+                <CarsElement
+                  carId={order.orderedCar.car}
+                  dateStart={order.dateStart}
+                  dateEnd={order.dateEnd}
+                  totalPrice={order.orderedCar.totalPrice}
+                />
+              </OrderDetailsItem>
+            ))
+          ) : (
+            <p className="order-details__empty">{t("order.empty")}</p>
+          )}
+        </>
       )}
     </div>
   );
