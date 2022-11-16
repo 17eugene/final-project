42dcd42ea3477c8877ba294d1309b6ab060a57ca
17eugene <drivercoua@gmail.com>
2022-11-16 15:43:57 +0200

    add forwardRef and inputRef

--- client/src/components/FormInput/FormInput.tsx
+++ client/src/components/FormInput/FormInput.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FocusEvent } from "react";
+import { ChangeEvent, FocusEvent, ForwardedRef, forwardRef } from "react";
 import "../../styles/FormInput/FormInput.scss";
 
 interface IFormInputProps {
@@ -11,39 +11,46 @@ interface IFormInputProps {
   variant?: string;
   readonly?: boolean;
   disabled?: boolean;
-  maxLength?: number
+  maxLength?: number;
+  // ref?: RefObject<HTMLInputElement> | null;
 }
 
-const FormInput = ({
-  name,
-  type,
-  placeholder,
-  value,
-  onChange,
-  onBlur,
-  variant,
-  readonly,
-  disabled,
-  maxLength
-}: IFormInputProps) => {
-  return (
-    <input
-      name={name}
-      type={type}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-      onBlur={onBlur}
-      autoComplete="off"
-      autoCorrect="off"
-      className={
-        variant === "authorization" ? "auth-form__input" : "form__input"
-      }
-      readOnly={readonly}
-      disabled={disabled}
-      maxLength={maxLength}
-    />
-  );
-};
+const FormInput = forwardRef(
+  (
+    {
+      name,
+      type,
+      placeholder,
+      value,
+      onChange,
+      onBlur,
+      variant,
+      readonly,
+      disabled,
+      maxLength,
+    }: IFormInputProps,
+    ref: ForwardedRef<HTMLInputElement>
+  ) => {
+    return (
+      <input
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        autoComplete="off"
+        autoCorrect="off"
+        className={
+          variant === "authorization" ? "auth-form__input" : "form__input"
+        }
+        readOnly={readonly}
+        disabled={disabled}
+        maxLength={maxLength}
+        ref={ref}
+      />
+    );
+  }
+);
 
 export default FormInput;
