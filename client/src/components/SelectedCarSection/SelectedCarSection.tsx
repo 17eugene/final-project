ac7d13e3316cd1b4d36cb3e8084279a9008d515f
17eugene <drivercoua@gmail.com>
2022-11-16 04:24:38 +0200

    loader implementation

--- client/src/components/SelectedCarSection/SelectedCarSection.tsx
+++ client/src/components/SelectedCarSection/SelectedCarSection.tsx
@@ -9,6 +9,7 @@ import CarFeaturesArea from "../CarFeaturesArea/CarFeaturesArea";
 import UpdateForm from "../UpdateForm/UpdateForm";
 import Backdrop from "../Backdrop/Backdrop";
 import DeleteConfirmation from "../DeleteConfirmation/DeleteConfirmation";
+import Loader from "../Loader/Loader";
 
 import "../../styles/SelectedCarSection/SelectedCarSection.scss";
 
@@ -18,9 +19,12 @@ const SelectedCarSection = () => {
   const [isOpenedDeleteConfirmation, setIsOpenedDeleteConfirmation] =
     useState<boolean>(false);
 
+  const [deleteError, setDeleteError] = useState<string>("");
+
   const cars = useAppSelector((state) => state.cars.carsCollection);
   const selectedCar = useAppSelector((state) => state.cars.selectedCar);
   const dispatch = useAppDispatch();
+  const isRefreshing = useAppSelector((state) => state.auth.refreshing);
 
   useEffect(() => {
     dispatch(getDefaultCar(selectedCar || cars[0]));
@@ -32,6 +36,7 @@ const SelectedCarSection = () => {
 
   const toggleDeleteConfirmation = useCallback(() => {
     setIsOpenedDeleteConfirmation(!isOpenedDeleteConfirmation);
+    setDeleteError("");
   }, [isOpenedDeleteConfirmation]);
 
   const toggleUpdateForm = useCallback(() => {
@@ -41,39 +46,55 @@ const SelectedCarSection = () => {
 
   const deleteCarHandler = useCallback(() => {
     if (selectedCar) {
-      dispatch(carsOperations.deleteCar({ _id: selectedCar._id }));
+      dispatch(carsOperations.deleteCar({ _id: selectedCar._id })).then(
+        (response) => {
+          if (response.payload?.message) {
+            setDeleteError("Error occured while delete!");
+          } else {
+            setDeleteError("");
+            setIsOpenedDeleteConfirmation(false);
+            setIsOpenedEdit(false);
+          }
+        }
+      );
     }
-    setIsOpenedEdit(false);
   }, [dispatch, selectedCar]);
 
   return (
     <div className="selected-car-wrapper">
-      {selectedCar && (
+      {isRefreshing ? (
+        <Loader />
+      ) : (
         <>
-          <CarViewArea />
-          {isOpenedUpdateForm ? (
+          {selectedCar && (
             <>
-              <Backdrop>
-                <div></div>
-              </Backdrop>
-              <UpdateForm toggleUpdateForm={toggleUpdateForm} />
+              <CarViewArea />
+              {isOpenedUpdateForm ? (
+                <>
+                  <Backdrop>
+                    <div></div>
+                  </Backdrop>
+                  <UpdateForm toggleUpdateForm={toggleUpdateForm} />
+                </>
+              ) : (
+                <CarFeaturesArea
+                  toggleDeleteConfirmation={toggleDeleteConfirmation}
+                  toggleEdit={toggleEdit}
+                  isOpenedEdit={isOpenedEdit}
+                  selectedCar={selectedCar}
+                  toggleUpdateForm={toggleUpdateForm}
+                />
+              )}
             </>
-          ) : (
-            <CarFeaturesArea
-              toggleDeleteConfirmation={toggleDeleteConfirmation}
-              // deleteCarHandler={deleteCarHandler}
-              toggleEdit={toggleEdit}
-              isOpenedEdit={isOpenedEdit}
-              selectedCar={selectedCar}
-              toggleUpdateForm={toggleUpdateForm}
-            />
           )}
         </>
       )}
+
       {isOpenedDeleteConfirmation && (
         <DeleteConfirmation
           toggleDeleteConfirmation={toggleDeleteConfirmation}
           deleteCarHandler={deleteCarHandler}
+          deleteError={deleteError}
         />
       )}
     </div>
